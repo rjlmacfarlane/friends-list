c97fa6e104b1c28844107b00ab79bea86d1a6f79
rjlmacfarlane <rjl.macfarlane@gmail.com>
2021-03-07 18:01:10 -0400

    Migrate to psql for deployment

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20210307203428) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "friends", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
@@ -24,7 +27,7 @@ ActiveRecord::Schema.define(version: 20210307203428) do
     t.integer  "user_id"
   end
 
-  add_index "friends", ["user_id"], name: "index_friends_on_user_id"
+  add_index "friends", ["user_id"], name: "index_friends_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -36,7 +39,7 @@ ActiveRecord::Schema.define(version: 20210307203428) do
     t.datetime "updated_at",                          null: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
